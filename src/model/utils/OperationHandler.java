8c7832571d117661ff10c8d4c9010f9e65ae6e4e
Mateus Lopez <mateusrlopez@gmail.com>
2019-12-18 15:34:13 -0300

    Alterações para optimização

--- src/model/utils/OperationHandler.java
+++ src/model/utils/OperationHandler.java
@@ -9,6 +9,8 @@ import javax.script.ScriptException;
 import javafx.scene.control.Label;
 
 public class OperationHandler implements Constants {
+	private static ScriptEngineManager manager = new ScriptEngineManager();
+	
 	private static Label lb1;
 	private static Label lb2;
 	private static Label lb3;
@@ -20,6 +22,7 @@ public class OperationHandler implements Constants {
 	private static String temporaryResult;
 	
 	public static void handleOperations(String op,boolean special){
+		String recentOperation = lb2.getText();
 		String currentOperand = lb1.getText();
 		char lastOperand;
 
@@ -40,27 +43,27 @@ public class OperationHandler implements Constants {
 				handleSpecialOperation(SpecialOP.get(op));
 			} else {
 				if (operationInProgress && InputHandler.isOverridible && !specialOperationInProgress) 
-					lb2.setText(lb2.getText().substring(0, lb2.getText().length() - 1) + op);
-				else if (specialOperationInProgress && !operationInProgress) {
-					lb2.setText(lb2.getText() + " " + op);
+					lb2.setText(recentOperation.substring(0, recentOperation.length() - 1) + op);
+				else if (operationInProgress && !InputHandler.isOverridible) {
+					lastOperand = recentOperation.charAt(recentOperation.length() - 1);
+					lb2.setText(recentOperation + String.format(" %s %s", currentOperand, op));
+					result = OperationHandler.processOperation(result + lastOperand + currentOperand);
+					lb1.setText(String.format("%s",result));
+					InputHandler.isOverridible = true;
+				} else if (specialOperationInProgress && !operationInProgress) {
+					lb2.setText(recentOperation + " " + op);
 					result = temporaryResult;
 					specialOperationInProgress = false;
 					operationInProgress = true;
 				} else if (specialOperationInProgress && operationInProgress) {
-					lastOperand = lb2.getText().split(" ")[lb2.getText().split(" ").length-2].charAt(0);
-					lb2.setText(lb2.getText() + String.format(" %s",op));
+					lastOperand = recentOperation.split(" ")[recentOperation.split(" ").length-2].charAt(0);
+					lb2.setText(recentOperation + String.format(" %s",op));
 					result = OperationHandler.processOperation(result + lastOperand + temporaryResult);
 					lb1.setText(String.format("%s",result));
 					InputHandler.isOverridible = true;
 					specialOperationInProgress = false;
-				} else if (operationInProgress && !InputHandler.isOverridible) {
-					lastOperand = lb2.getText().charAt(lb2.getText().length() - 1);
-					lb2.setText(lb2.getText() + String.format(" %s %s", currentOperand, op));
-					result = OperationHandler.processOperation(result + lastOperand + currentOperand);
-					lb1.setText(String.format("%s",result));
-					InputHandler.isOverridible = true;
 				} else {
-					lb2.setText(lb2.getText() + String.format(" %s %s", currentOperand, op));
+					lb2.setText(recentOperation + String.format(" %s %s", currentOperand, op));
 					result = currentOperand;
 					operationInProgress = true;
 					InputHandler.isOverridible = true;
@@ -77,16 +80,17 @@ public class OperationHandler implements Constants {
 	}
 	
 	public static void handleEquals(){
+		String recentOperation = lb2.getText();
 		if (lb2.getText().equals("")) 
 			InputHandler.isOverridible = true;
 		else {
 			try {
 				if (operationInProgress && !specialOperationInProgress)
-					result = processOperation(result + lb2.getText().charAt(lb2.getText().length() - 1) + lb1.getText()).toString();
+					result = processOperation(result + recentOperation.charAt(recentOperation.length() - 1) + lb1.getText()).toString();
 				else if (specialOperationInProgress && !operationInProgress)
 					result = temporaryResult;
 				else {
-					char lastOperand = lb2.getText().split(" ")[lb2.getText().split(" ").length-2].charAt(0);
+					char lastOperand = recentOperation.split(" ")[recentOperation.split(" ").length-2].charAt(0);
 					result = processOperation(result + lastOperand + temporaryResult).toString();
 				}
 				lb2.setText("");
@@ -106,7 +110,6 @@ public class OperationHandler implements Constants {
 	}
 
 	private static String processOperation(String operation) throws ScriptException {
-		ScriptEngineManager manager = new ScriptEngineManager();
 		ScriptEngine engine = manager.getEngineByName("javascript");
 		Object result = engine.eval(operation);
 		if (!result.toString().matches(Regex1))
@@ -137,8 +140,9 @@ public class OperationHandler implements Constants {
 	}
 	
 	private static void handleMultipleSpecialOperations(String s) {
-		String txt = lb2.getText().split(" ")[lb2.getText().split(" ").length - 1];
-		StringBuilder sb = new StringBuilder(lb2.getText());
+		String recentOperation = lb2.getText();
+		String txt = recentOperation.split(" ")[recentOperation.split(" ").length - 1];
+		StringBuilder sb = new StringBuilder(recentOperation);
 		int index = sb.lastIndexOf(" ");
 		sb.delete(index,sb.length());
 		sb.append(String.format(" %s(%s)",s,txt));
